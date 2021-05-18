845e488b0abb9c024f1a0b65133406815729c871
Soraja Sekavcnik <soraja.sekavcnik@student.fmf.uni-lj.si>
2021-05-18 14:39:16 +0200

    dodana SKRIVNOST in poravki /nova-igra/

--- vislice.py
+++ vislice.py
@@ -1,27 +1,32 @@
 import bottle
 import model
 
-vislice = model.Vislice()
+SKRIVNOST = 'moja skrivnost'
+
+vislice = model.Vislice(model.DATOTEKA_S_STANJEM)
 
 @bottle.get('/') # / pomeni korensko mapo na strežniku
 def index():
     return bottle.template('index.tpl')
 
-@bottle.post('/igra/')
+@bottle.post('/nova-igra/')
 def nova_igra():
     id_igre = vislice.nova_igra()
-    bottle.redirect('/igra/{}/'.format(id_igre))
+    bottle.response.set_cookie('idigre', 'idigre{}'.format(id_igre), path='/', secret=SKRIVNOST) # nastavili smo piškotek, 7 pomeni da mamo pousot dostop do cookija
+    bottle.redirect('/igra/')
 
-@bottle.get('/igra/<id_igre:int>/')
-def pokazi_igro(id_igre):
+@bottle.get('/igra/')
+def pokazi_igro():
+    id_igre = int(bottle.request.get_cookie('idigre', secret=SKRIVNOST).split('e')[1]) #hočmo dobit številko k je pr idigre, zato razdelimo to pr e in uzamemo samo število, to je split in [1] 
     igra, stanje = vislice.igre[id_igre]
-    return bottle.template('igra.tpl', igra=igra, id_igre=id_igre, stanje=stanje)
+    return bottle.template('igra.tpl', igra=igra, stanje=stanje)
 
-@bottle.post('/igra/<id_igre:int>/')
-def ugibaj(id_igre):
+@bottle.post('/igra/')
+def ugibaj():
+    id_igre = int(bottle.request.get_cookie('idigre', secret=SKRIVNOST).split('e')[1])
     crka = bottle.request.forms.getunicode('crka')
     vislice.ugibaj(id_igre, crka)
-    bottle.redirect('/igra/{}/'.format(id_igre))
+    bottle.redirect('/igra/')
 
 @bottle.get('/img/<picture>') # v <> smo poimenoval vse slike skp s picture
 def serve_pictures(picture):
